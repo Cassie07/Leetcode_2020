ac6bb1fe6d99842b510e5fc126fa6f5245078004
Kexin Ding <31313145+Cassie07@users.noreply.github.com>
2020-05-30 11:39:57 -0400

    medium

--- 24.SwapNodeInPair.py
+++ 24.SwapNodeInPair.py
@@ -1,17 +1,34 @@
 class Solution:
     def swapPairs(self, head: ListNode) -> ListNode:
+#         step = 0
+#         root = cur = ListNode(0)
+#         root.next = head
+#         slow = head
+#         while slow:
+#             step += 1
+#             if step % 2 == 1:
+#                 continue
+#             else:
+#                 fast = slow.next
+#                 if fast == None:
+#                     return root.next
+#                 slow.next = fast.next
+#                 fast.next = slow
+#                 cur.next = fast
+#                 cur = slow
+#                 slow = slow.next
+#         return root.next
         step = 0
-        root = cur = ListNode(0)
+        root = ListNode(0)
         root.next = head
+        cur = root
         slow = head
-        while slow:
+        while slow and slow.next:
             step += 1
-            if step % 2 == 1:
+            if step%2 == 1:
                 continue
             else:
                 fast = slow.next
-                if fast == None:
-                    return root.next
                 slow.next = fast.next
                 fast.next = slow
                 cur.next = fast
