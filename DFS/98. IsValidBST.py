022fce12d29a9cdbf610e9feaf2a88cdee09d2c7
Kexin Ding <31313145+Cassie07@users.noreply.github.com>
2020-06-01 16:50:10 -0400

    medium

---
+++ DFS/98. IsValidBST.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isValidBST(self, root: TreeNode) -> bool:
+        # DFS
+        if not root:
+            return True
+        
+        stack = [(root, float('-inf'),float('inf'))]
+        while stack:
+            root, lower, upper = stack.pop()
+            if root == None:
+                continue
+            
+            if root.val <= lower or root.val >= upper:
+                return False
+            
+            stack.append((root.left, lower, root.val))
+            stack.append((root.right, root.val, upper))
+            
+        return True 
