6d7c06e95518a742517f275d1b29ac52971af83f
Kexin Ding <31313145+Cassie07@users.noreply.github.com>
2020-06-04 00:22:03 -0400

    Create 543. DiameterOfBinaryTree.py

---
+++ Tree/543. DiameterOfBinaryTree.py
@@ -0,0 +1,15 @@
+class Solution:
+    def diameterOfBinaryTree(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        self.best = 0
+        def depth(node):
+            if not node:
+                return 0
+            L = depth(node.left)
+            R = depth(node.right)
+            self.best = max(self.best, L+R)
+            return max(L,R) + 1
+        
+        depth(root)
+        return self.best
