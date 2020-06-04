3c2dddf32ea07da25eea324e0a9e505d92a2bbba
Kexin Ding <31313145+Cassie07@users.noreply.github.com>
2020-06-04 11:10:08 -0400

    Update 543. DiameterOfBinaryTree.py

--- Tree/543. DiameterOfBinaryTree.py
+++ Tree/543. DiameterOfBinaryTree.py
@@ -8,8 +8,8 @@ class Solution:
                 return 0
             L = depth(node.left)
             R = depth(node.right)
-            self.best = max(self.best, L+R)
-            return max(L,R) + 1
+            self.best = max(self.best, L+R) 
+            return max(L,R) + 1 # max(L,R) (previous) + 1(new)
         
         depth(root)
         return self.best
