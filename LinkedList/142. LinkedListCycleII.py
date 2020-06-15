8012c1cd6afba99c901983b87731391c221d88be
Kexin Ding <31313145+Cassie07@users.noreply.github.com>
2020-06-14 21:17:42 -0400

    Update 142. LinkedListCycleII.py

--- LinkedList/142. LinkedListCycleII.py
+++ LinkedList/142. LinkedListCycleII.py
@@ -2,7 +2,7 @@ class Solution:
     def detectCycle(self, head: ListNode) -> ListNode:
         if head == None:
             return head
-        # 1. intersection
+        # 1. has cycle?
         slow = fast = head
         while fast.next and fast.next.next:
             slow = slow.next
@@ -12,7 +12,7 @@ class Solution:
         else: # fast.next.next/ fast.next == None
             return None
                   
-        # 2. 
+        # 2. find the start(end) of the circle
         slow = head
         while slow != fast:
             slow = slow.next
